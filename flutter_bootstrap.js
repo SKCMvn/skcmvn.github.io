643547e47c7e2f0263313e33fd457a4c1f32ce9e
thang <thanglhssi@gmail.com>
2024-12-03 15:43:22 +0700

    release

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b26e1b023cdb35e49952ebd6cf59bbdf23785a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3535929264"
+    serviceWorkerVersion: "3507720623"
   }
 });
